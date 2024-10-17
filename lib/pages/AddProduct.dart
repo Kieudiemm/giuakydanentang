1524a9f75f331ae80bc37f0c4b1f9edb346bc9f9
Kieudiemm <nguyenthikieudien347@gmail.com>
2024-10-17 07:43:03 +0700

    Updated AddProduct page

--- lib/pages/AddProduct.dart
+++ lib/pages/AddProduct.dart
@@ -10,9 +10,9 @@ class Addproduct extends StatefulWidget {
 }
 
 class _AddproductState extends State<Addproduct> {
-  TextEditingController tenspController = new TextEditingController();
-  TextEditingController loaispController = new TextEditingController();
-  TextEditingController giaspController = new TextEditingController();
+  TextEditingController tenspController = TextEditingController();
+  TextEditingController loaispController = TextEditingController();
+  TextEditingController giaspController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -25,88 +25,92 @@ class _AddproductState extends State<Addproduct> {
             Row(
               children: [
                 GestureDetector(
-                  onTap: (){
+                  onTap: () {
                     Navigator.pop(context);
                   },
-                    child: Icon(Icons.arrow_back_outlined)),
-                const SizedBox(
-                  width: 80.0,
+                  child: const Icon(Icons.arrow_back_outlined),
                 ),
+                const SizedBox(width: 80.0),
                 const Text(
                   "Thêm sản phẩm",
                   style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.bold),
+                    color: Colors.black,
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ],
             ),
-            const SizedBox(
-              height: 30.0,
-            ),
+            const SizedBox(height: 30.0),
             const Text(
               "Tên sản phẩm: ",
               style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.w500),
+                color: Colors.black,
+                fontSize: 24.0,
+                fontWeight: FontWeight.w500,
+              ),
             ),
             Container(
               padding: const EdgeInsets.only(left: 20.0),
               decoration: BoxDecoration(
-                  color: const Color(0xFFececf8),
-                  borderRadius: BorderRadius.circular(10)),
+                color: const Color(0xFFececf8),
+                borderRadius: BorderRadius.circular(10),
+              ),
               child: TextField(
                 controller: tenspController,
                 decoration: const InputDecoration(
-                    border: InputBorder.none, hintText: "Nhập tên sản phẩm"),
+                  border: InputBorder.none,
+                  hintText: "Nhập tên sản phẩm",
+                ),
               ),
             ),
-            const SizedBox(
-              height: 30.0,
-            ),
+            const SizedBox(height: 30.0),
             const Text(
               "Loại sản phẩm: ",
               style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.w500),
+                color: Colors.black,
+                fontSize: 24.0,
+                fontWeight: FontWeight.w500,
+              ),
             ),
             Container(
               padding: const EdgeInsets.only(left: 20.0),
               decoration: BoxDecoration(
-                  color: const Color(0xFFececf8),
-                  borderRadius: BorderRadius.circular(10)),
+                color: const Color(0xFFececf8),
+                borderRadius: BorderRadius.circular(10),
+              ),
               child: TextField(
                 controller: loaispController,
                 decoration: const InputDecoration(
-                    border: InputBorder.none, hintText: "Nhập loại sản phẩm"),
+                  border: InputBorder.none,
+                  hintText: "Nhập loại sản phẩm",
+                ),
               ),
             ),
-            const SizedBox(
-              height: 30.0,
-            ),
+            const SizedBox(height: 30.0),
             const Text(
               "Giá sản phẩm: ",
               style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.w500),
+                color: Colors.black,
+                fontSize: 24.0,
+                fontWeight: FontWeight.w500,
+              ),
             ),
             Container(
               padding: const EdgeInsets.only(left: 20.0),
               decoration: BoxDecoration(
-                  color: const Color(0xFFececf8),
-                  borderRadius: BorderRadius.circular(10)),
+                color: const Color(0xFFececf8),
+                borderRadius: BorderRadius.circular(10),
+              ),
               child: TextField(
                 controller: giaspController,
                 decoration: const InputDecoration(
-                    border: InputBorder.none, hintText: "Nhập giá sản phẩm"),
+                  border: InputBorder.none,
+                  hintText: "Nhập giá sản phẩm",
+                ),
               ),
             ),
-            const SizedBox(
-              height: 40.0,
-            ),
+            const SizedBox(height: 40.0),
             GestureDetector(
               onTap: () async {
                 if (tenspController.text != "" &&
@@ -121,12 +125,13 @@ class _AddproductState extends State<Addproduct> {
                   await DatabaseMethods()
                       .themsp(thongtinsp, addID)
                       .then((value) {
-                        tenspController.text="";
-                        loaispController.text="";
-                        giaspController.text="";
+                    tenspController.text = "";
+                    loaispController.text = "";
+                    giaspController.text = "";
                     ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
                       backgroundColor: Colors.red,
-                        content: Text("Dữ liệu sản phẩm đã được cập nhật")));
+                      content: Text("Dữ liệu sản phẩm đã được cập nhật"),
+                    ));
                   });
                 }
               },
@@ -135,20 +140,22 @@ class _AddproductState extends State<Addproduct> {
                   padding: const EdgeInsets.symmetric(vertical: 10.0),
                   width: 100,
                   decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(10)),
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
                   child: const Center(
                     child: Text(
                       "Thêm",
                       style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 24.0,
-                          fontWeight: FontWeight.w500),
+                        color: Colors.black,
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.w500,
+                      ),
                     ),
                   ),
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
